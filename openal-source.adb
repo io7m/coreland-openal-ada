1a89bb9322024ce88d2f24443618343eff2c6bd1
User <m0@viper.internal.network>
2010-01-06 09:51:49 +0000

    Much more of the Sources API

--- openal-source.adb
+++ openal-source.adb
@@ -1,3 +1,5 @@
+with OpenAL.Thin;
+
 package body OpenAL.Source is
 
   use type Thin.Size_t;
@@ -22,5 +24,207 @@ package body OpenAL.Source is
   begin
     return Boolean (Thin.Is_Source (Thin.Unsigned_Integer_t (Source)));
   end Is_Valid;
+
+  procedure Set_Positioning
+    (Source   : in Source_ID_t;
+     Relative : in Boolean) is
+  begin
+    Thin.Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_SOURCE_RELATIVE,
+       Value     => Thin.Integer_t (Boolean'Pos (Relative)));
+  end Set_Positioning;
+
+  procedure Get_Positioning
+    (Source   : in     Source_ID_t;
+     Relative :    out Boolean)
+  is
+    Value : aliased Thin.Integer_t;
+  begin
+    Thin.Get_Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_SOURCE_RELATIVE,
+       Value     => Value'Address);
+    Relative := Boolean'Val (Value);
+  end Get_Positioning;
+
+  procedure Set_Type
+    (Source      : in Source_ID_t;
+     Source_Type : in Source_Type_t)
+  is
+    Value : Thin.Integer_t;
+  begin
+    case Source_Type is
+      when Undetermined => Value := Thin.AL_UNDETERMINED;
+      when Static       => Value := Thin.AL_STATIC;
+      when Streaming    => Value := Thin.AL_STREAMING;
+    end case;
+
+    Thin.Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_SOURCE_TYPE,
+       Value     => Value);
+  end Set_Type;
+
+  procedure Get_Type
+    (Source      : in     Source_ID_t;
+     Source_Type :    out Source_Type_t)
+  is
+    Value : aliased Thin.Integer_t;
+  begin
+    Thin.Get_Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_SOURCE_TYPE,
+       Value     => Value'Address);
+
+    case Value is
+      when Thin.AL_UNDETERMINED => Source_Type := Undetermined;
+      when Thin.AL_STATIC       => Source_Type := Static;
+      when Thin.AL_STREAMING    => Source_Type := Streaming;
+      when others =>
+        raise Constraint_Error with "invalid value returned from alGetSourcei";
+    end case;
+  end Get_Type;
+
+  procedure Set_Looping
+    (Source  : in Source_ID_t;
+     Looping : in Boolean) is
+  begin
+    Thin.Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_LOOPING,
+       Value     => Thin.Integer_t (Boolean'Pos (Looping)));
+  end Set_Looping;
+
+  procedure Get_Looping
+    (Source  : in     Source_ID_t;
+     Looping :    out Boolean)
+  is
+    Value : aliased Thin.Integer_t;
+  begin
+    Thin.Get_Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_LOOPING,
+       Value     => Value'Address);
+    Looping := Boolean'Val (Value);
+  end Get_Looping;
+
+  procedure Set_Current_Buffer
+    (Source : in Source_ID_t;
+     Buffer : in OpenAL.Buffer.Buffer_ID_t) is
+  begin
+    Thin.Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_BUFFER,
+       Value     => Thin.Integer_t (OpenAL.Buffer.To_Integer (Buffer)));
+  end Set_Current_Buffer;
+
+  procedure Get_Current_Buffer
+    (Source : in     Source_ID_t;
+     Buffer :    out OpenAL.Buffer.Buffer_ID_t)
+  is
+    Value : aliased Thin.Unsigned_Integer_t;
+  begin
+    Thin.Get_Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_BUFFER,
+       Value     => Value'Address);
+    Buffer := OpenAL.Buffer.From_Integer (Value);
+  end Get_Current_Buffer;
+
+  procedure Get_Buffers_Queued
+    (Source  : in     Source_ID_t;
+     Buffers :    out Natural)
+  is
+    Value : aliased Thin.Unsigned_Integer_t;
+  begin
+    Thin.Get_Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_BUFFERS_QUEUED,
+       Value     => Value'Address);
+    Buffers := Natural (Value);
+  end Get_Buffers_Queued;
+
+  procedure Get_Buffers_Processed
+    (Source  : in     Source_ID_t;
+     Buffers :    out Natural)
+  is
+    Value : aliased Thin.Unsigned_Integer_t;
+  begin
+    Thin.Get_Sourcei
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_BUFFERS_QUEUED,
+       Value     => Value'Address);
+    Buffers := Natural (Value);
+  end Get_Buffers_Processed;
+
+  procedure Set_Minimum_Gain
+    (Source : in Source_ID_t;
+     Gain   : in Gain_t) is
+  begin
+    Thin.Sourcef
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_MIN_GAIN,
+       Value     => Thin.Float_t (Gain));
+  end Set_Minimum_Gain;
+
+  procedure Get_Minimum_Gain
+    (Source : in     Source_ID_t;
+     Gain   :    out Gain_t)
+  is
+    Value : aliased Gain_t;
+  begin
+    Thin.Get_Sourcef
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_MIN_GAIN,
+       Value     => Value'Address);
+    Gain := Value;
+  end Get_Minimum_Gain;
+
+  procedure Set_Maximum_Gain
+    (Source : in Source_ID_t;
+     Gain   : in Gain_t) is
+  begin
+    Thin.Sourcef
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_MIN_GAIN,
+       Value     => Thin.Float_t (Gain));
+  end Set_Maximum_Gain;
+
+  procedure Get_Maximum_Gain
+    (Source : in     Source_ID_t;
+     Gain   :    out Gain_t)
+  is
+    Value : aliased Gain_t;
+  begin
+    Thin.Get_Sourcef
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_MIN_GAIN,
+       Value     => Value'Address);
+    Gain := Value;
+  end Get_Maximum_Gain;
+
+  procedure Set_Pitch
+    (Source : in Source_ID_t;
+     Pitch  : in Pitch_t) is
+  begin
+    Thin.Sourcef
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_PITCH,
+       Value     => Thin.Float_t (Pitch));
+  end Set_Pitch;
+
+  procedure Get_Pitch
+    (Source : in     Source_ID_t;
+     Pitch  :    out Pitch_t)
+  is
+    Value : aliased Pitch_t;
+  begin
+    Thin.Get_Sourcef
+      (Source_ID => Thin.Unsigned_Integer_t (Source),
+       Parameter => Thin.AL_PITCH,
+       Value     => Value'Address);
+    Pitch := Value;
+  end Get_Pitch;
 
 end OpenAL.Source;
--- openal-source.ads
+++ openal-source.ads
@@ -1,4 +1,5 @@
-with OpenAL.Thin;
+with OpenAL.Buffer;
+with OpenAL.Types;
 
 package OpenAL.Source is
 
@@ -24,8 +25,94 @@ package OpenAL.Source is
   -- proc_map : alIsSource
   function Is_Valid (Source : in Source_ID_t) return Boolean;
 
+  -- proc_map : alSource
+  procedure Set_Positioning
+    (Source   : in Source_ID_t;
+     Relative : in Boolean);
+
+  -- proc_map : alGetSource
+  procedure Get_Positioning
+    (Source   : in     Source_ID_t;
+     Relative :    out Boolean);
+
+  type Source_Type_t is (Undetermined, Static, Streaming);
+
+  -- proc_map : alSource
+  procedure Set_Type
+    (Source      : in Source_ID_t;
+     Source_Type : in Source_Type_t);
+
+  -- proc_map : alGetSource
+  procedure Get_Type
+    (Source      : in     Source_ID_t;
+     Source_Type :    out Source_Type_t);
+
+  -- proc_map : alSource
+  procedure Set_Looping
+    (Source   : in Source_ID_t;
+     Looping  : in Boolean);
+
+  -- proc_map : alGetSource
+  procedure Get_Looping
+    (Source   : in     Source_ID_t;
+     Looping  :    out Boolean);
+
+  -- proc_map : alSource
+  procedure Set_Current_Buffer
+    (Source : in Source_ID_t;
+     Buffer : in OpenAL.Buffer.Buffer_ID_t);
+
+  -- proc_map : alGetSource
+  procedure Get_Current_Buffer
+    (Source : in     Source_ID_t;
+     Buffer :    out OpenAL.Buffer.Buffer_ID_t);
+
+  -- proc_map : alGetSource
+  procedure Get_Buffers_Queued
+    (Source  : in     Source_ID_t;
+     Buffers :    out Natural);
+
+  -- proc_map : alGetSource
+  procedure Get_Buffers_Processed
+    (Source  : in     Source_ID_t;
+     Buffers :    out Natural);
+
+  subtype Gain_t is Types.Float_t range 0.0 .. 1.0;
+
+  -- proc_map : alSource
+  procedure Set_Minimum_Gain
+    (Source : in Source_ID_t;
+     Gain   : in Gain_t);
+
+  -- proc_map : alGetSource
+  procedure Get_Minimum_Gain
+    (Source : in     Source_ID_t;
+     Gain   :    out Gain_t);
+
+  -- proc_map : alSource
+  procedure Set_Maximum_Gain
+    (Source : in Source_ID_t;
+     Gain   : in Gain_t);
+
+  -- proc_map : alGetSource
+  procedure Get_Maximum_Gain
+    (Source : in     Source_ID_t;
+     Gain   :    out Gain_t);
+
+  subtype Pitch_t is Types.Float_t range 0.00001 .. Types.Float_t'Last;
+
+  -- proc_map : alSource
+  procedure Set_Pitch
+    (Source : in Source_ID_t;
+     Pitch  : in Pitch_t);
+
+  -- proc_map : alGetSource
+  procedure Get_Pitch
+    (Source : in     Source_ID_t;
+     Pitch  :    out Pitch_t);
+
 private
 
-  type Source_ID_t is new Thin.Unsigned_Integer_t;
+  type Source_ID_t is new Types.Unsigned_Integer_t;
 
 end OpenAL.Source;
