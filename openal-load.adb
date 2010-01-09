14fbceed07e35e249b1a2b0ccbe13292da183129
m0 <m0@viper>
2010-01-09 18:45:35 +0000

    Use conversion in Load_Subprogram

--- openal-load.adb
+++ openal-load.adb
@@ -1,17 +1,32 @@
+with Ada.Unchecked_Conversion;
 with Interfaces.C;
 with System;
 
 package body OpenAL.Load is
   package C renames Interfaces.C;
 
-  function Load_Subprogram (Name : in String) return Subprogram_Access_t is
+  function Load_Subprogram (Name : in String) return Subprogram_Access_Type is
+    pragma Assert (Subprogram_Access_Type'Size = System.Address'Size);
+
     function Get_Proc_Address
-      (Name : System.Address) return Subprogram_Access_t;
+      (Name : System.Address) return System.Address;
     pragma Import (C, Get_Proc_Address, "alGetProcAddress");
 
-    C_Name : aliased C.char_array := C.To_C (Name);
+    function Convert is new Ada.Unchecked_Conversion
+      (Source => System.Address,
+       Target => Subprogram_Access_Type);
+
+    use type System.Address;
+
+    C_Name  : aliased C.char_array := C.To_C (Name);
+    Address : constant System.Address :=
+      Get_Proc_Address (C_Name (C_Name'First)'Address);
   begin
-    return Get_Proc_Address (C_Name (C_Name'First)'Address);
+    if Address = System.Null_Address then
+      raise Load_Error with "subprogram " & Name & " not found";
+    end if;
+
+    return Convert (Address);
   end Load_Subprogram;
 
 end OpenAL.Load;
--- openal-load.ads
+++ openal-load.ads
@@ -1,8 +1,10 @@
-generic
-  type Subprogram_Access_t is private;
-
 package OpenAL.Load is
 
-  function Load_Subprogram (Name : in String) return Subprogram_Access_t;
+  Load_Error : exception;
+
+  generic
+    type Subprogram_Access_Type is private;
+
+  function Load_Subprogram (Name : in String) return Subprogram_Access_Type;
 
 end OpenAL.Load;
