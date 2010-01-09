dff7b5043bfe234c518fa8bd9589da775741201b
m0 <m0@viper>
2010-01-09 11:07:35 +0000

    Add blank line

--- openal-extension-efx_thin.sh
+++ openal-extension-efx_thin.sh
@@ -6,6 +6,7 @@ cat openal-extension-efx_thin.ads.0 >> openal-extension-efx_thin.ads.tmp || exit
 
 ./openal-mkconst.lua openal_efx_1_1_const.dat >> openal-extension-efx_thin.ads.tmp || exit 1
 
+echo >> openal-extension-efx_thin.ads.tmp || exit 1
 ./block-comment 'OpenAL EFX' >> openal-extension-efx_thin.ads.tmp || exit 1
 ./openal-mkapi.lua "al" openal_efx_1_1_types.dat openal_efx_1_1_names.dat openal_types.dat >> openal-extension-efx_thin.ads.tmp || exit 1
 
