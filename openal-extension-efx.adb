c4bbe29e5589280b24b5af9d13032258d402c4cb
m0 <m0@viper>
2010-01-10 07:46:11 +0000

    Initial EFX subprograms

--- openal-extension-efx.adb
+++ openal-extension-efx.adb
@@ -1,13 +1,11 @@
---with OpenAL.Load;
-
 package body OpenAL.Extension.EFX is
 
---  procedure Check_Loaded (Extension : in Extension_t) is
---  begin
---    if Extension.Loaded = False then
---      raise Load.Load_Error with "extension not loaded";
---    end if;
---  end Check_Loaded;
+  procedure Check_Loaded (Extension : in Extension_t) is
+  begin
+    if Extension.Loaded = False then
+      raise Program_Error with "extension not loaded";
+    end if;
+  end Check_Loaded;
 
   function Is_Present (Device : in Context.Device_t) return Boolean is
   begin
@@ -22,5 +20,111 @@ package body OpenAL.Extension.EFX is
       (API    => EFX_Thin.Load_API,
        Loaded => True);
   end Load_Extension;
+
+  --
+  -- Effects.
+  --
+
+  procedure Generate_Effects
+    (Extension : in     Extension_t;
+     Effects   : in out Effect_Array_t) is
+  begin
+    Check_Loaded (Extension);
+
+    Extension.API.Gen_Effects
+      (Size    => Effects'Length,
+       Effects => Effects (Effects'First)'Address);
+  end Generate_Effects;
+
+  procedure Delete_Effects
+    (Extension : in Extension_t;
+     Effects   : in Effect_Array_t) is
+  begin
+    Check_Loaded (Extension);
+
+    Extension.API.Delete_Effects
+      (Size    => Effects'Length,
+       Effects => Effects (Effects'First)'Address);
+  end Delete_Effects;
+
+  function Effect_Is_Valid
+    (Extension : in Extension_t;
+     Effect    : in Effect_t) return Boolean is
+  begin
+    Check_Loaded (Extension);
+
+    return Boolean (Extension.API.Is_Effect (Types.Unsigned_Integer_t (Effect)));
+  end Effect_Is_Valid;
+
+  --
+  -- Filters.
+  --
+
+  procedure Generate_Filters
+    (Extension : in     Extension_t;
+     Filters   : in out Filter_Array_t) is
+  begin
+    Check_Loaded (Extension);
+
+    Extension.API.Gen_Filters
+      (Size    => Filters'Length,
+       Filters => Filters (Filters'First)'Address);
+  end Generate_Filters;
+
+  procedure Delete_Filters
+    (Extension : in Extension_t;
+     Filters   : in Filter_Array_t) is
+  begin
+    Check_Loaded (Extension);
+
+    Extension.API.Delete_Filters
+      (Size    => Filters'Length,
+       Filters => Filters (Filters'First)'Address);
+  end Delete_Filters;
+
+  function Filter_Is_Valid
+    (Extension : in Extension_t;
+     Filter    : in Filter_t) return Boolean is
+  begin
+    Check_Loaded (Extension);
+
+    return Boolean (Extension.API.Is_Filter (Types.Unsigned_Integer_t (Filter)));
+  end Filter_Is_Valid;
+
+  --
+  -- Auxiliary_Effect_Slots.
+  --
+
+  procedure Generate_Auxiliary_Effect_Slots
+    (Extension : in     Extension_t;
+     Slots     : in out Auxiliary_Effect_Slot_Array_t) is
+  begin
+    Check_Loaded (Extension);
+
+    Extension.API.Gen_Auxiliary_Effect_Slots
+      (Size  => Slots'Length,
+       Slots => Slots (Slots'First)'Address);
+  end Generate_Auxiliary_Effect_Slots;
+
+  procedure Delete_Auxiliary_Effect_Slots
+    (Extension : in Extension_t;
+     Slots     : in Auxiliary_Effect_Slot_Array_t) is
+  begin
+    Check_Loaded (Extension);
+
+    Extension.API.Delete_Auxiliary_Effect_Slots
+      (Size  => Slots'Length,
+       Slots => Slots (Slots'First)'Address);
+  end Delete_Auxiliary_Effect_Slots;
+
+  function Auxiliary_Effect_Slot_Is_Valid
+    (Extension : in Extension_t;
+     Slot      : in Auxiliary_Effect_Slot_t) return Boolean is
+  begin
+    Check_Loaded (Extension);
+
+    return Boolean
+      (Extension.API.Is_Auxiliary_Effect_Slot (Types.Unsigned_Integer_t (Slot)));
+  end Auxiliary_Effect_Slot_Is_Valid;
 
 end OpenAL.Extension.EFX;
--- openal-extension-efx.ads
+++ openal-extension-efx.ads
@@ -1,4 +1,5 @@
 with OpenAL.Context;
+with OpenAL.Types;
 
 private with OpenAL.Extension.EFX_Thin;
 
@@ -10,7 +11,91 @@ package OpenAL.Extension.EFX is
 
   function Load_Extension return Extension_t;
 
+  --
+  -- Effects.
+  --
+
+  type Effect_t       is private;
+  type Effect_Array_t is array (Positive range <>) of Effect_t;
+
+  No_Effect : constant Effect_t;
+
+  -- proc_map : alGenEffects
+  procedure Generate_Effects
+    (Extension : in     Extension_t;
+     Effects   : in out Effect_Array_t);
+
+  -- proc_map : alDeleteEffects
+  procedure Delete_Effects
+    (Extension : in Extension_t;
+     Effects   : in Effect_Array_t);
+
+  -- proc_map : alIsEffect
+  function Effect_Is_Valid
+    (Extension : in Extension_t;
+     Effect    : in Effect_t) return Boolean;
+
+  --
+  -- Filters.
+  --
+
+  type Filter_t       is private;
+  type Filter_Array_t is array (Positive range <>) of Filter_t;
+
+  No_Filter : constant Filter_t;
+
+  -- proc_map : alGenFilters
+  procedure Generate_Filters
+    (Extension : in     Extension_t;
+     Filters   : in out Filter_Array_t);
+
+  -- proc_map : alDeleteFilters
+  procedure Delete_Filters
+    (Extension : in Extension_t;
+     Filters   : in Filter_Array_t);
+
+  -- proc_map : alIsFilter
+  function Filter_Is_Valid
+    (Extension : in Extension_t;
+     Filter    : in Filter_t) return Boolean;
+
+  --
+  -- Auxiliary_Effect_Slots.
+  --
+
+  type Auxiliary_Effect_Slot_t       is private;
+  type Auxiliary_Effect_Slot_Array_t is array (Positive range <>) of Auxiliary_Effect_Slot_t;
+
+  No_Auxiliary_Effect_Slot : constant Auxiliary_Effect_Slot_t;
+
+  -- proc_map : alGenAuxiliaryEffectSlots
+  procedure Generate_Auxiliary_Effect_Slots
+    (Extension : in     Extension_t;
+     Slots     : in out Auxiliary_Effect_Slot_Array_t);
+
+  -- proc_map : alDeleteAuxiliaryEffectSlots
+  procedure Delete_Auxiliary_Effect_Slots
+    (Extension : in Extension_t;
+     Slots     : in Auxiliary_Effect_Slot_Array_t);
+
+  -- proc_map : alIsAuxiliaryEffectSlot
+  function Auxiliary_Effect_Slot_Is_Valid
+    (Extension : in Extension_t;
+     Slot      : in Auxiliary_Effect_Slot_t) return Boolean;
+
 private
+
+  type Effect_t is new Types.Unsigned_Integer_t;
+
+  No_Effect : constant Effect_t := 0;
+
+  type Filter_t is new Types.Unsigned_Integer_t;
+
+  No_Filter : constant Filter_t := 0;
+
+  type Auxiliary_Effect_Slot_t is new Types.Unsigned_Integer_t;
+
+  No_Auxiliary_Effect_Slot : constant Auxiliary_Effect_Slot_t := 0;
 
   type Extension_t is record
     Loaded : Boolean := False;
